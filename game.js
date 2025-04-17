f7f23fc2b2fbacce7dce7fb98150087911216e24
root <root@DESKTOP-AN4RLL7.>
2025-04-17 02:31:52 +0000

    Subiendo prototipo de Bichox Conquest

---
+++ game.js
@@ -0,0 +1,19 @@
+document.getElementById('left').addEventListener('click', function() {
+    alert('Movimiento hacia la izquierda');
+});
+
+document.getElementById('right').addEventListener('click', function() {
+    alert('Movimiento hacia la derecha');
+});
+
+document.getElementById('up').addEventListener('click', function() {
+    alert('Movimiento hacia arriba');
+});
+
+document.getElementById('down').addEventListener('click', function() {
+    alert('Movimiento hacia abajo');
+});
+
+document.getElementById('attack').addEventListener('click', function() {
+    alert('Atacar!');
+});
